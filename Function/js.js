883d88e9b56be5fce1ca2b6d3abcf5a969c637ef
Handsome <lukyanenkoaw@gmail.com>
2020-01-14 14:33:44 +0200

    work with EventListener

--- Function/js.js
+++ Function/js.js
@@ -1,40 +1,29 @@
-// let num = 20;
+let btn = document.querySelectorAll('button'),
+    wrap = document.querySelector('.wrapper'),
+    link = document.querySelector('a');
 
-// function showFirstMessage (text) {
-//     alert(text);
-//     let num = 10;
-//     console.log(num);
+// btn[0].onclick = function() {
+//     alert('You press BTN')
+// };
 
-// }
+// btn[0].addEventListener('click', function(event) {
+//     console.log('Произошло событие: ' + event.type + ' на элементе ' + event.target);
+// });
 
-// showFirstMessage("Hello world!");
-// console.log(num);
+// wrap.addEventListener('click', function(){
+//     console.log('Произошло событие: ' + event.type + ' на элементе ' + event.target);
+// })
 
+link.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log('Произошло событие: ' + event.type + ' на элементе ' + event.target);
+})
 
-let calc = (a,b) => a+b
-
-console.log(calc(3,4));
-
-console.log(calc(6,8));
-
-
-
-function retVar () {
-    let num = 50;
-    return num;
-}
-
-let anotherNum = retVar();
-console.log(anotherNum);
-
-let str = "test";
-console.log(str.length);
-
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-
-let twelve = '12.2px';
-
-// console.log(Math.round(twelve));
-
-console.log(parseFloat(twelve));
+btn.forEach(function(item) {
+    item.addEventListener('mouseleave', function() {
+        console.log('qwerty');
+    })
+})
+// btn[0].addEventListener('mouseenter', function() {
+//     alert('you enter on BTN');
+// });
